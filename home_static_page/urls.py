d617d1810bdcfeeea84e14b24ba0671b6cda3e81
Noussair <aouadinoussair@gmail.com>
2024-11-08 10:53:50 +0100

    adjustment the navbar and urls (home and about ) . created page About Us

--- home_static_page/urls.py
+++ home_static_page/urls.py
@@ -1,7 +1,8 @@
 # home_page_static/urls.py
 from django.urls import path
-from .views import HomePageView
+from .views import *
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
+    path('about/', AboutPageView.as_view(), name='about_page'),
 ]
