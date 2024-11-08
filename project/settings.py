387eb4201e0e29880c86a6003efdcf8525e18b0d
Noussair <aouadinoussair@gmail.com>
2024-11-08 05:06:59 +0100

    Reglage Static files and Media FIles

--- project/settings.py
+++ project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/var/www/static/",
+#    "/var/www/static/",
 ]
 
 #Media  files 
