845556d257ee2bd6ac4bb6f817b16af1cad2757b
Noussair <aouadinoussair@gmail.com>
2024-11-08 11:34:53 +0100

    Add pages  Contact , faqs,

--- home_static_page/views.py
+++ home_static_page/views.py
@@ -10,3 +10,10 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name= 'contact.html'
+    
+class FaqPageView(TemplateView):
+    template_name= 'faq.html'
